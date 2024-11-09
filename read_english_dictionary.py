22db867f0361fa981fd04fe31c43727d35527f50
Freddy Marten <martenfreddy@gmail.com>
2024-11-09 00:56:04 +0000

    initial commit

---
+++ read_english_dictionary.py
@@ -0,0 +1,12 @@
+# https://github.com/dwyl/english-words?tab=readme-ov-file
+import json
+from random import sample
+
+f = open('data/words_dictionary.json')
+
+d = json.load(f)
+
+# print(d)
+
+print(d)
+# hello
